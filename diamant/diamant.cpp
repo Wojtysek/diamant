27947ba3cca29f1ade56752db6b731722690f9fe
Vojtech Hruby <vojtech.hruby.020@pslib.cz>
2021-12-19 18:01:12 +0100

    cely diamant

--- diamant/diamant.cpp
+++ diamant/diamant.cpp
@@ -3,10 +3,10 @@
 int main(void) {
 	//diamant
 
-	int mezera = 10;
+	int mezera = 9;
 
 	//vlevo
-	for (int j = 0; j < 10; j++) {
+	for (int j = 0; j <= 9; j++) {
 
 		for (int i = 0; i < mezera; i++) {
 			printf(" ");
@@ -21,9 +21,25 @@ int main(void) {
 		printf("\n");
 		mezera--;
 	}
-	mezera = 0;
+	mezera = 1; //LG-ho nápad
 
 	//vpravo
+	for (int j = 8; j >= 0; j--) {
+		for (int i = 0; i < mezera; i++) {
+			printf(" ");
 
+		}
+		for (int k = j; k >= 1; k--) {
+			printf("%d", k);
+		}
+
+		for (int k = 2; k <= j; k++) {
+			printf("%d", k);
+		}
+
+
+		printf("\n");
+		mezera++;
+	}
 }
 
