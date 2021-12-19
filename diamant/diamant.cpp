fd4e1bfff02b7ce864e95b1d0d59cb310bce5915
Vojtech Hruby <vojtech.hruby.020@pslib.cz>
2021-12-19 16:39:26 +0100

    Přidejte soubory projektu.

---
+++ diamant/diamant.cpp
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	//nasobilka
+	for (int j = 1; j <= 10; j++) {
+		
+		for (int i = 1; i <= 10; i++) {
+			printf("%4d", i * j);
+		}
+		printf("\n");
+	}
+	//diamant
+	//sikma plocha(horní 2)
+	for (int j = 1; j <= 9; j++) {
+
+		for (int i = 1; i <= j; i++) {
+			printf("%d ", i);
+		}
+		printf("\n");
+	}
+	//sikma plocha(dolni 2)
+	for (int j = 8; j >= 1; j--) {
+
+		for (int i = 1; i <= j; i++) {
+			printf("%d ", i);
+		}
+		printf("\n");
+	}
+	//sikma plocha(horni 1)
+	for (int j = 1; j >= 9; j++) {
+
+		for (int i = 0; j <= 1; j++) {
+			printf("%d ", i);
+		}
+	}
+}
+
